c12fcb1662e182f11a79938d7da4393675f4deea
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-02-13 15:20:26 -0800

    add passing snapshot tests for Face, Controls, and Moods components

--- src/components/face/Face.test.js
+++ src/components/face/Face.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Face from './Face';
+import { Provider } from 'react-redux';
+import store from '../../store';
 
 describe('Face component', () => {
   it('renders Face', () => {
-    const wrapper = shallow(<Face />);
+    const wrapper = shallow(<Provider store={store} ><Face /></Provider>);
     expect(wrapper).toMatchSnapshot();
   });
 });
